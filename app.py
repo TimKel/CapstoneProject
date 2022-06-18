a5b80508cf86597d330e0f290b796639978523e1
Tim Kelley <timkelley683@gmail.com>
2022-06-17 17:10:07 -0700

    Adjusted config

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "spotitsecrets")
 toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
